f5c83395f0681664ef508c4e12bb43bee1acb01f
HiroHsu <hermit.hong@gmail.com>
2022-05-14 12:29:32 +0800

    Update master.sh

--- Ubuntu 20.04/RunOnMaster/master.sh
+++ Ubuntu 20.04/RunOnMaster/master.sh
@@ -3,15 +3,14 @@
 wget -O crio.sh https://raw.githubusercontent.com/HiroHsu/UK8s/main/Ubuntu%2020.04/CRIO1.24/crio.sh 
 wget -O K8s.sh https://raw.githubusercontent.com/HiroHsu/UK8s/main/Ubuntu%2020.04/K8s1.24/K8s.sh 
 
-source ./crio.sh
-source ./K8s.sh
+sh ./crio.sh
+sh ./K8s.sh
 
 kubeadm init
-export KUBECONFIG=/etc/kubernetes/admin.conf
 
-whoami
-exit
-echo "Exit root"
+sudo -s <<EOF
+export KUBECONFIG=/etc/kubernetes/admin.conf
+EOF
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
