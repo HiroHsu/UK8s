96be1767a59290c729d93c5702b61a4f0f97918f
HiroHsu <hermit.hong@gmail.com>
2022-05-14 19:29:19 +0800

    Update crio.sh

--- Ubuntu 20.04/CRIO1.24/crio.sh
+++ Ubuntu 20.04/CRIO1.24/crio.sh
@@ -34,6 +34,8 @@ sudo sysctl --system
 export VERSION=1.24
 export OS=xUbuntu_20.04
 
+sudo -s <<EOF
+
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
@@ -45,3 +47,5 @@ sudo apt-get install cri-o cri-o-runc
 
 systemctl daemon-reload
 systemctl start crio
+
+EOF
